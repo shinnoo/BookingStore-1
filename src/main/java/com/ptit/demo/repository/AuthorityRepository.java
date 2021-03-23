f4151d9f81bd4bc2c79be68351d5cf87e859d07f
trandung <trandung164.ptit@gmail.com>
2021-03-23 07:38:34 +0700

    Initial version of BookStore generated by JHipster-6.10.0

---
+++ src/main/java/com/ptit/demo/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.ptit.demo.repository;
+
+import com.ptit.demo.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
